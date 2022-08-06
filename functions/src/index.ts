f0257f1710f648232e33106177443c6a6073cdc6
Michael May <mskmay66@gmail.com>
2022-08-06 12:00:16 -0700

    correct cloud function (i think, untested)

---
+++ functions/src/index.ts
@@ -0,0 +1,26 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+const db = admin.firestore();
+const fcm = admin.messaging();
+
+export const sendToToken = functions.firestore
+       .document("userData/{userId}/matches/{matchId}/messages/{messageId}")
+       .onCreate(async snapshot => {
+       const message = snapshot.data();
+
+       const querySnapshot = await db.collection("userData").doc(message.target).collection("tokens").get();
+
+       const tokens = querySnapshot.docs.map(snap => snap.id);
+
+       const payload: admin.messaging.MessagingPayload = {
+       notification: {
+            title: 'New message from ${message.sender}'
+            body: message.message
+            clickAction: "FLUTTER_NOTIFICATION_CLICK"
+         }
+       };
+       fcm.sendToDevice(tokens, payload);
+     });
+
