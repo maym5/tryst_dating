90348b16c6e2f50298cdf9a324bc2f18873af661
Michael May <mskmay66@gmail.com>
2022-08-24 11:23:48 -0700

    added new date cloud functions (haven't tested)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,11 +5,11 @@ admin.initializeApp();
 const db = admin.firestore();
 const fcm = admin.messaging();
 
-export const sendToTokens = functions.firestore
+export const sendChat = functions.firestore
     .document("userData/{userId}/matches/{matchId}/messages/{messageId}")
     .onCreate(async (snapshot, context) => {
       const message = snapshot.data();
-      if (context.params.userId != message.target) {
+      if (context.params.userId == message.target) {
         const querySnapshot = await db.collection("userData")
             .doc(message.target).collection("tokens").get();
 
@@ -24,6 +24,27 @@ export const sendToTokens = functions.firestore
           notification: {
             title: targetData?.name,
             body: message.message,
+            clickAction: "FLUTTER_NOTIFICATION_CLICK",
+          },
+        };
+        fcm.sendToDevice(tokens, payload);
+      }
+    });
+
+export const newDate = functions.firestore
+    .document("userData/{userId}/matches/{matchId}")
+    .onCreate(async (snapshot, context) => {
+      const date = snapshot.data();
+      if (context.params.userId == date.matchUID) {
+        const querySnapshot = await db.collection("userData")
+            .doc(date.matchUID).collection("tokens").get();
+
+        const tokens = querySnapshot.docs.map((snap) => snap.id);
+
+        const payload: admin.messaging.MessagingPayload = {
+          notification: {
+            title: "You've got a date with ${date.name}",
+            body: "Check it out in app",
             clickAction: "FLUTTER_NOTIFICATION_CLICK",
           },
         };
