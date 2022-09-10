e40c73395599379541a93d78a51d6e086d583b5f
Michael May <mskmay66@gmail.com>
2022-09-10 12:43:52 -0700

    actual clean build

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -164,23 +164,7 @@ class UserData with ChangeNotifier {
     UserImages.getImagesFromUserData();
   }
 
-  // Future<bool> uploadUserLocation() async {
-  //   final FirebaseFirestore _db = FirebaseFirestore.instance;
-  //   User? _user = retrieveUser();
-  //   try {
-  //     await _db.collection("userData").doc(_user?.uid).update({
-  //       "latitude": UserData.location!.latitude,
-  //       "longitude": UserData.location!.longitude,
-  //     });
-  //     return true;
-  //   } catch (e) {
-  //     return false;
-  //   }
-  // }
-
   Future<void> setLocation() async {
     UserData.location = await UserData.userLocation;
-    // uploadUserLocation()
-    //     .then((value) => value ? null : throw ("couldn't upload location"));
   }
 }
